65c122c4e2bd7205bacf7e5d7f4efda5f31d1076
Jefferson Neves <jefferson.rpn@gmail.com>
2020-07-09 16:16:28 -0300

    Cria páginas internas

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AnalisesComponent } from './analises/analises.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { MidiaComponent } from './midia/midia.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,18 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'analises',
+    component: AnalisesComponent
+  },
+  {
+    path: 'midia',
+    component: MidiaComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
   }
 ];
 
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 
 import { MainRoutingModule } from './main-routing.module';
+import { AnalisesComponent } from './analises/analises.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { MidiaComponent } from './midia/midia.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AnalisesComponent,
+    SobreComponent,
+    MidiaComponent
   ],
   imports: [
     CommonModule,
